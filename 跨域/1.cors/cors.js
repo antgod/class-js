90b43cd8ec57ab1d3c51b4b624f42af7c835244f
李宏吉 <lihongji@meituan.com>
2016-05-22 20:36:23 +0800

    add

--- 跨域/1.cors/cors.js
+++ 跨域/1.cors/cors.js
@@ -1,13 +1,16 @@
-var http=require("http");
+var http = require("http");
 
-var server=http.createServer(waiter);
+var server = http.createServer(waiter);
 
-function waiter(req,rsp){
-    rsp.setHeader('Access-Control-Allow-Credentials', "true");
-    rsp.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+function waiter(req, res) {
 
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);//注意这里不能使用 *
+    res.setHeader('Access-Control-Allow-Credentials', true);//告诉客户端可以在HTTP请求中带上Cookie
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.writeHead(200, {
+        'Content-Type': 'text/html;charset=utf8'
+    });
+    res.end("end");
 
-    rsp.end("end");
 }
-
-server.listen(3000,"localhost");
+server.listen(3000, "localhost");
